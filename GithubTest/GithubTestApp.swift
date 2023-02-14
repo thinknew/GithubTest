e2d9625199d9148527a416f98740edf0f2349925
thinknew <avinashsingh214@gmail.com>
2023-02-15 09:50:25 +1100

    Initial Commit

---
+++ GithubTest/GithubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubTestApp.swift
+//  GithubTest
+//
+//  Created by Avinash Singh on 15/2/2023.
+//
+
+import SwiftUI
+
+@main
+struct GithubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
